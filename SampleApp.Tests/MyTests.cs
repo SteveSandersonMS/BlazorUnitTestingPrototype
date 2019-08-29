02813d231892a0d089820578fa5b5dea9caa3155
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2019-08-29 09:05:45 +0100

    Prototype

---
+++ SampleApp.Tests/MyTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Components.Testing;
+using SampleApp.Pages;
+using System;
+using Xunit;
+
+namespace SampleApp.Tests
+{
+    public class MyTests
+    {
+        private TestHost host = new TestHost();
+
+        [Fact]
+        public void CounterWorks()
+        {
+            var component = host.AddComponent<Counter>();
+            Func<string> countValue = () => component.Find("#count").InnerText;
+
+            Assert.Equal("Counter", component.Find("h1").InnerText);
+            Assert.Equal("Current count: 0", countValue());
+
+            component.Find("button.inc").Click();
+            Assert.Contains("Current count: 1", countValue());
+
+            component.Find("button.dec").Click();
+            Assert.Contains("Current count: 0", countValue());
+        }
+
+        [Fact]
+        public void FetchDataWorks()
+        {
+            // Initially shows loading state
+            var req = host.AddMockHttp().Capture("/sample-data/weather.json");
+            var component = host.AddComponent<FetchData>();
+            Assert.Contains("Loading...", component.GetMarkup());
+
+            // When the server responds, we display the data
+            host.WaitForNextRender(() => req.SetResult(new[]
+            {
+                new FetchData.WeatherForecast { Summary = "First" },
+                new FetchData.WeatherForecast { Summary = "Second" },
+            }));
+            Assert.DoesNotContain("Loading...", component.GetMarkup());
+            Assert.Collection(component.FindAll("tbody tr"),
+                row => Assert.Contains("First", row.OuterHtml),
+                row => Assert.Contains("Second", row.OuterHtml));
+        }
+    }
+}
---
+++ SampleApp.Tests/TodoListTest.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Components.Testing;
+using SampleApp.Pages;
+using Xunit;
+
+namespace SampleApp.Tests
+{
+    public class TodoListTest
+    {
+        private TestHost host = new TestHost();
+
+        [Fact]
+        public void InitiallyDisplaysNoItems()
+        {
+            var component = host.AddComponent<TodoList>();
+            var items = component.FindAll("li");
+            Assert.Empty(items);
+        }
+
+        [Fact]
+        public void CanAddItems()
+        {
+            // Arrange
+            var component = host.AddComponent<TodoList>();
+
+            // Act
+            component.Find("input").Change("First item");
+            component.Find("form").Submit();
+
+            component.Find("input").Change("Second item");
+            component.Find("form").Submit();
+
+            // Assert
+            Assert.Collection(component.FindAll("li span"),
+                li => Assert.Equal("First item", li.InnerText),
+                li => Assert.Equal("Second item", li.InnerText));
+            Assert.Empty(component.Find("input").GetAttributeValue("value", string.Empty));
+        }
+
+        [Fact]
+        public void CanRemoveItems()
+        {
+            // Arrange
+            var component = host.AddComponent<TodoList>();
+            component.Find("input").Change("First item");
+            component.Find("form").Submit();
+            component.Find("input").Change("Second item");
+            component.Find("form").Submit();
+
+            // Act
+            component.Find("li .delete").Click();
+
+            // Assert
+            Assert.Collection(component.FindAll("li span"),
+                li => Assert.Equal("Second item", li.InnerText));
+        }
+    }
+}
